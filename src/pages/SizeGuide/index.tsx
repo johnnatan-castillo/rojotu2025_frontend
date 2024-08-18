f9ad4f1e7d4a4df18c23b30fa2d46786932360ac
fab-henao <fabianfeik474@gmail.com>
2024-08-17 23:08:27 -0500

    guia de tallas imagenes

--- src/pages/SizeGuide/index.tsx
+++ src/pages/SizeGuide/index.tsx
@@ -4,6 +4,13 @@ import CustomClass from '../../utils/CustomClass'
 import VIDEOMASCULINO from "../../assets/videos/desk-guia-de-tallas-hombre.mp4"
 import VIDEOFEMENINO from "../../assets/videos/desk-guia-de-tallas-mujer.mp4"
 
+// Guia de tallas
+import WOMANGUIDE_1 from "../../assets/side-guide/MUJER.png"
+
+import MENGUIDE_1 from "../../assets/side-guide/HOMBRE-1.png"
+import MENGUIDE_2 from "../../assets/side-guide/HOMBRE-2.png"
+// Guia de tallas
+
 import 'plyr/dist/plyr.css'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../redux/store'
@@ -25,23 +32,32 @@ const SizeGuide = () => {
         </div>
         <div className={`${CustomClass({ component, version, customClass: "size-guide-box-2" })}`}>
           <div className={`${CustomClass({ component, version, customClass: "size-guide-box-2-web-size-guide" })}`}>
-            
+
             {
               gender === "FEMENINO" &&
-              <video autoPlay={true} loop={true} playsInline={true} muted className={`${CustomClass({ component, version, customClass: "size-guide-box-2-web-size-guide-video" })}`} controls>
-              <source src={VIDEOFEMENINO} type="video/mp4" />
-              
-              Tu navegador no soporta el elemento de video.
-            </video>
+              <>
+                <video autoPlay={true} loop={true} playsInline={true} muted className={`${CustomClass({ component, version, customClass: "size-guide-box-2-web-size-guide-video" })}`} controls>
+                  <source src={VIDEOFEMENINO} type="video/mp4" />
+
+                  Tu navegador no soporta el elemento de video.
+                </video>
+
+                <img className={`${CustomClass({ component, version, customClass: "img-size-guide" })}`} src={WOMANGUIDE_1} alt="guia de tallas" />
+              </>
             }
 
-{
+            {
               gender === "MASCULINO" &&
-              <video autoPlay={true} loop={true} playsInline={true} muted className={`${CustomClass({ component, version, customClass: "size-guide-box-2-web-size-guide-video" })}`} controls>
-              <source src={VIDEOMASCULINO} type="video/mp4" />
-              
-              Tu navegador no soporta el elemento de video.
-            </video>
+              <>
+                <video autoPlay={true} loop={true} playsInline={true} muted className={`${CustomClass({ component, version, customClass: "size-guide-box-2-web-size-guide-video" })}`} controls>
+                  <source src={VIDEOMASCULINO} type="video/mp4" />
+
+                  Tu navegador no soporta el elemento de video.
+                </video>
+
+                <img className={`${CustomClass({ component, version, customClass: "img-size-guide" })}`} src={MENGUIDE_1} alt="guia de tallas" />
+                <img className={`${CustomClass({ component, version, customClass: "img-size-guide" })}`} src={MENGUIDE_2} alt="guia de tallas" />
+              </>
             }
 
           </div>
